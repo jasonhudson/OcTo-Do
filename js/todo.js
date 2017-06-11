7838a7e2f0b459e0bda12e09ee1454e849285556
jasonhudson <jason@shuttleworthfoundation.org>
2017-06-11 09:21:21 +0200

    Basic functionality and style added

--- js/todo.js
+++ js/todo.js
@@ -2,24 +2,18 @@ angular.module('ToDo', []).
 controller('todoController', ['$scope', function($scope) {
     $scope.todos = [{
         'title': 'Build a todo app',
-        'description': 'Write a basic Angular todo list',
         'done': false
     }];
 
     $scope.addTodo = function() {
-            $scope.todos.push({
+        $scope.todos.push({
                 'title': $scope.newTodo,
-                'description': $scope.description,
                 'done': false
             })
-            $scope.newTodo = ''
-            $scope.description = ''
-        }
-        // function to delete todo item
-    $scope.deleteTodos = function(start) {
-        // start is the position where we want to stater deleting
-        $scope.todos.splice(start, 1);
+            // make sure our input box clears after adding a todo
+        $scope.newTodo = ''
     }
+
     $scope.clearTodos = function() {
         $scope.todos = $scope.todos.filter(function(item) {
             return !item.done
